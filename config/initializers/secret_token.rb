ba2c1ef6a7e87ef1acc0e312b63e1934496a6b8e
Francisco Sandoval <franciscosandoval15@gmail.com>
2013-05-01 14:57:46 -0400

    #953110 Proyecto Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CUALimpieza::Application.config.secret_token = '33c06b29fe1616b0313ce05dc77b91f21a2e68a3cfb339d7a1648017e21bd772a225ededff349d890d79375daab70b8c95885df483eef614cf0df87fc0a0e924'
