c1e36fe9fa93fc80773b975ca4c509e097905d09
Francisco Sandoval <franciscosandoval15@gmail.com>
2013-05-02 03:04:35 -0400

    #953117 Control de sesion base (falta cambiar la pagina inicial cuando si estoy autenticado)

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: FranciscoJavier
+ * Date: 02-05-13
+ * Time: 12:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,51 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :current_user, :set_controller_and_action, :session_control
+  force_ssl
+
+  def set_controller_and_action
+    @current_action = action_name
+    @current_controller = controller_name
+  end
+
+#---------------------------------------- Session Methods ----------------------------------------
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def session_control
+    if !public_sites && !signed_in?
+      session_end
+      redirect_to :controller=>'home', :action => 'index'
+    elsif public_sites && signed_in?
+      # redirect_to :controller=>'cleaning', :action => 'index'    #CAMBIAR POR PRINCIPAL DE LIMPIEZA
+    end
+  end
+
+  def signed_in?
+    !@current_user.nil?
+  end
+
+  def public_sites
+    #Sitios en los que se puede estar sin estar autenticado
+    #En Home:
+    @is_public_site = (@current_controller=='home')
+  end
+
+  def session_restart(user)
+    session_end
+    session_start(user)
+  end
+
+  def session_start(user)
+    @current_user = user
+    session[:user_id] = user.id
+  end
+
+  def session_end
+    @current_user = nil
+    reset_session
+  end
+#-------------------------------------- END Session Methods --------------------------------------
+
 end
