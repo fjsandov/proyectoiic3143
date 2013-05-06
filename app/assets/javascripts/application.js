95f1d95326ac95ae10ae720ea87223ca9112f184
Pablo Cruz <pcruzm@gmail.com>
2013-05-05 21:41:07 -0400

    Creado layout base (bootstrap) #953114, #952802, #953119.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require html5shiv
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+//= require twitter/bootstrap
 //= require bootstrap
 //= require_tree .
