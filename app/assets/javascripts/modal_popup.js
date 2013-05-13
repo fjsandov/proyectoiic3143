a349974f8bf2a2cd8ee5c528489ca3712c2d20f6
Francisco Sandoval <franciscosandoval15@gmail.com>
2013-05-13 01:43:00 -0400

    #953124  Formularios en popup modal hacen submit por ajax y hacen render de la respuesta en el mismo modal. Falta manejar error de request ajax y cerrar modal cuando formulario se proceso sin problemas (o mostrar un mensaje avisando el exito).

--- app/assets/javascripts/modal_popup.js
+++ app/assets/javascripts/modal_popup.js
@@ -1,24 +1,48 @@
-
 $(
     function(){
 
-        var modal_popup =  $('#modal-popup');
+        var $modal_popup =  $('#modal-popup');
 
-        modal_popup.on('shown', function () {
+        $modal_popup.on('shown', function () {
             /*---Aplica datepicker JQUERY UI a quien tenga la clase datepicker dentro del contenido cargado en el popup---*/
                 var datepickers = $(".datepicker");
                 if(datepickers.length>0){
                     datepickers.datepicker({ dateFormat: 'dd-mm-yy' });
                 }
             /*------------------------------------------------------------------------------------------------------------*/
-        })
+
+            /*----------------------------COMPORTAMIENTO DE FORMULARIOS EN MODAL--------------------------------*/
+            var frm = $('form',$modal_popup);
+            frm.submit(function (event) {
+                //Evita que ocurra el comportamiento por defecto.
+                event.preventDefault();
+                $.ajax({
+                    type: frm.attr('method'),
+                    url: frm.attr('action'),
+                    data: frm.serialize(),
+                    success: function(data) {
+                        $modal_popup.removeData('modal');
+                        $('.modal-body').replaceWith(data);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown){
+                        // log the error to the console
+                        console.error("OCURRIO EL SIGUIENTE ERROR: " + textStatus, errorThrown);
+                    }
+                });
+            });
+        });
+        /*----------------------------FIN COMPORTAMIENTO DE FORMULARIOS EN MODAL--------------------------------*/
+
 
         /*---------------------PARA QUE AL CARGAR UN 2° MODAL POPUP, SE VUELVAN A PEDIR LOS DATOS---------------------*/
-        modal_popup.on('hidden',function(){
+        $modal_popup.on('hidden',function(){
             $(this).removeData('modal');
             var default_modal_body = '<div class="modal-body"><img class = "loading-img" src="/assets/images/cargando.gif"></div>';
             $('.modal-body').replaceWith(default_modal_body);
         });
         /*-------------------------------------------------------------------------------------------------------------------------*/
+
+
+
     }
 )
