e00766acc27eb72910ac9212be23290157c1dab4
Francisco Sandoval <franciscosandoval15@gmail.com>
2013-05-16 17:29:17 -0400

    #953112 v0.1 datos de prueba (salas y sectores)

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,15 @@ s4 = Sector.create(name: 'Recepcion')
 
 r1 = Room.create(name: 'M01', floor: 2, status: 'free', sector_id: s1.id)
 r2 = Room.create(name: 'M02', floor: 2, status: 'free', sector_id: s1.id)
-r3 = Room.create(name: 'P01', floor: 1, status: 'free', sector_id: s3.id)
-r4 = Room.create(name: 'R01', floor: 3, status: 'free', sector_id: s4.id)
+r3 = Room.create(name: 'M03', floor: 2, status: 'free', sector_id: s1.id)
+r4 = Room.create(name: 'M04', floor: 2, status: 'free', sector_id: s1.id)
+r5 = Room.create(name: 'M05', floor: 2, status: 'free', sector_id: s1.id)
+r6 = Room.create(name: 'P01', floor: 3, status: 'free', sector_id: s3.id)
+r7 = Room.create(name: 'R01', floor: 1, status: 'free', sector_id: s4.id)
+r8 = Room.create(name: 'R02', floor: 1, status: 'free', sector_id: s4.id)
+r9 = Room.create(name: 'R03', floor: 1, status: 'free', sector_id: s4.id)
+r10 = Room.create(name: 'R04', floor: 1, status: 'free', sector_id: s4.id)
+
 
 
 
