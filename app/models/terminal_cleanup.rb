955f8d5140a71fea7bf90d313f196f98d45571df
Pablo Cruz <pcruzm@gmail.com>
2013-05-22 20:19:42 -0400

    Agregado Limpiezas Terminales en vista General #953120

--- app/models/terminal_cleanup.rb
+++ app/models/terminal_cleanup.rb
@@ -8,7 +8,76 @@ class TerminalCleanup < ActiveRecord::Base
 
 
   def self.get_today_instances
-    nil  #TODO: esperar a lo que pablo va a hacer con calendario para hacer este metodo
+    self.get_instances(Time.zone.today, Time.zone.today + 1.day)
+  end
+
+  # Retorna un arreglo de diccionarios con info simple sobre cada aseo terminal agendado
+  # para la fecha dada.
+  def self.get_instances(start_date, end_date)
+    today = Date.today.to_time_in_current_zone
+    events = []
+
+    TerminalCleanup.find_each do |tc|
+      rule_date = tc.start_date
+      # Primera fecha debe ser >= start_date
+      while rule_date < start_date
+        rule_date += tc.interval.days  # TODO: calc. iterativo no es eficiente
+      end
+
+      # Cachear tci para evitar demasiadas consultas
+      tci_collection = tc.terminal_cleanup_instances.where('(original_date BETWEEN ? AND ?) OR ' +
+                                                           '(instance_date BETWEEN ? AND ?) OR ' +
+                                                           'instance_date IS NULL',
+                                                           start_date, end_date, start_date, end_date)
+
+      # capturar los instances que fueron considerados por siguiente while (Si comienzo con arreglo vacio Rails se marea)
+      tci_returned_ids = [0]
+
+      rule_end_date = (tc.end_date or end_date)
+      while rule_date <= rule_end_date
+        # check terminal_cleanup_instances
+        tci = tci_collection.find { |tc_instance| tc_instance.original_date == rule_date }
+        if tci
+          tci_returned_ids << tci.id
+          if tci.instance_date && tci.instance_date <= end_date
+            # generado a partir de una instancia
+            events << {
+                :eventType => 'instance',
+                :id => tci.id,
+                :start => tci.instance_date,
+                :title => tci.terminal_cleanup.room.name,
+            }
+          else
+            # es una excepcion de aseo
+          end
+        else
+          # generado a partir de la regla
+          events << {
+              :eventType => 'rule',
+              :id => tc.id,
+              :start => rule_date,
+              :title => tc.room.name,
+          }
+        end
+
+        rule_date += tc.interval.days
+      end
+
+      # revisar los ids que no fueron considerados en el while anterior
+      tci_collection.each do |tci|
+        if (not tci.id.in? tci_returned_ids) && tci.instance_date && tci.instance_date.between?(start_date, end_date)
+          # generado a partir de una instancia
+          events << {
+              :eventType => 'instance',
+              :id => tci.id,
+              :start => tci.instance_date,
+              :title => tci.terminal_cleanup.room.name,
+          }
+        end
+      end
+    end # end find_each
+
+    return events
   end
 
 end
